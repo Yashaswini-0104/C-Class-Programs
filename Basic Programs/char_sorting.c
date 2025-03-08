b4005f7420582269c80fabc8ae22358ebb7b02d0
Yashaswini-0104 <134691846+Yashaswini-0104@users.noreply.github.com>
2025-03-08 15:30:33 +0530

    Add files via upload

---
+++ Basic Programs/char_sorting.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main()
+{
+    char c[30], t;
+    int flag =0, count=0;
+    printf("Enter the string to be sorted:\n");
+    scanf("%s", c);
+    int n=strlen(c);
+    for(int i=0; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            count++;
+            if(c[i]>c[j]){
+                t = c[i];
+                c[i] = c[j];
+                c[j] = t;
+            }
+        }
+    }
+    printf("\nThe sorted string is %s:", c);
+    return 0;
+}
